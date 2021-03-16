418e94f1366819282a53928f2d388a87f0135f80
Walysson-nzxt <walyssondosreis@email.com>
2021-03-15 22:23:42 -0300

    Inicial

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-# This is a sample Python script.
+# TITLE: WALLet
+# DESCRIPTION: Gestão pessoal de finanças.
+# AUTHOR: WALYSSON PEREIRA DOS REIS
+# CREATE DATA: 21.03.15
+# UPDATE DATA: 21.03.15
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# coding: utf-8
+
+class PivoAcao:
+    """  Estabelecimento ou Pessoa de Origem ou Destino do recurso """
+    pass
+
+class
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
